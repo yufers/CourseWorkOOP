9534113ce0943cce481803039c0708aa03a93646
yufers <Anton.m.lotos@gmail.com>
2025-04-01 14:12:44 +0300

    Add home page

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from CourseWork!\n";
-}
+#include "crow.h"
 
+int main(int, char **)
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([]() {
+        auto page = crow::mustache::load("index.html");
+        crow::mustache::context ctx;
+
+        return page.render(ctx); });
+    
+    CROW_ROUTE(app, "/login/<string>")([](std::string name) {
+        auto page = crow::mustache::load("index.html");
+        crow::mustache::context ctx({{"person", name}});
+
+        return page.render(ctx); });
+        
+    app.port(18080).multithreaded().run();
+}
