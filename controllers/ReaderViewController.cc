15eb53f5ced5c299a3d63ed6b12ff6be8aeb7ee2
yufers <Anton.m.lotos@gmail.com>
2025-04-21 17:52:51 +0300

    add list of readers

---
+++ controllers/ReaderViewController.cc
@@ -0,0 +1,35 @@
+#include <drogon/HttpSimpleController.h>
+#include <drogon/HttpResponse.h>
+#include <drogon/orm/Mapper.h>
+
+#include <vector>
+
+#include "../models/Readers.h"
+
+using namespace drogon;
+using namespace drogon::orm;
+using namespace drogon_model::library;
+
+class ReaderViewController : public HttpSimpleController<ReaderViewController>
+{
+public:
+    PATH_LIST_BEGIN
+    PATH_ADD("/readers");
+    PATH_LIST_END
+
+    void asyncHandleHttpRequest(
+        const HttpRequestPtr &req,
+        std::function<void(const HttpResponsePtr &)> &&callback) override
+    {
+        auto clientPtr = drogon::app().getDbClient();
+
+        Mapper<Readers> mpReaders(clientPtr);
+        auto readers = mpReaders.orderBy(Readers::Cols::_reader_num).offset(0).findAll();
+
+        HttpViewData data;
+        data["title"] = "Список пользователей";
+        data["readers"] = readers;
+        auto resp = HttpResponse::newHttpViewResponse("ListReaders", data);
+        callback(resp);
+    }
+};
